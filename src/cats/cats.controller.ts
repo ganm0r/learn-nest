51c1a69ead742cfb2443a1a0c31b9e1455d7d845
Gandharv More <gandharv.more@pens.com>
2023-09-12 14:06:00 +0530

    explore post, httpcode, header decorators

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Header } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action adds a new cat';
   }
 }
